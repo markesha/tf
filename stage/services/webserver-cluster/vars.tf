150a6f981547fe2bb3e4823123cfb1a85f15461d
vlad <vlad>
2017-12-21 02:23:51 +0000

    add webserver-cluster module

--- stage/services/webserver-cluster/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  default = "8080"
-}
-
-variable "elb_port" {
-  description = "The elb port for HTTP"
-  default = "80"
-}
